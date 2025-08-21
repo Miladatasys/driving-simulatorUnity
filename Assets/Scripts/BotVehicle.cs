25c55244a3b22c55110aa438937820aae5762da1
Mila Data <camilamorales.it@gmail.com>
2025-08-20 21:32:11 -0400

    Feat: Camera switch, oncoming cars, Player movement

---
+++ Assets/Scripts/BotVehicle.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BotVehicle : MonoBehaviour
+{
+    private float speed = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We move the vehicles forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float speed = 15.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal Input
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
     }
 }
